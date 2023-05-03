45ded813c22669ed49ec63f444ce624effbe1a55
denish mistry <denish.mistry@indianic.com>
2023-05-03 16:43:52 +0530

    improve project sec js

--- assets/js/custom_withcomment.js
+++ assets/js/custom_withcomment.js
@@ -160,11 +160,11 @@ function BrainImage() {
 // page transition
 // ================
 const $header = document.querySelector("header");
-// const $main = document.querySelector(".profile-page");
+const $main = document.querySelector(".profile-page");
 const $logo = document.querySelector(".transition__logo");
 const $frameBlack = document.querySelector(".page-transition__black");
 const $frameRed = document.querySelector(".page-transition__seondary");
-// const $button = document.querySelector("#button");
+const $button = document.querySelector("#button");
 
 let tltransition = new TimelineMax({
     paused: true,
@@ -205,14 +205,14 @@ let tltransition = new TimelineMax({
   .set($frameRed, {
     scaleX: 0,
   })
-  // .fromTo(
-  //   $main, {
-  //     autoAlpha: 0,
-  //   }, {
-  //     autoAlpha: 1,
-  //     ease: Power4.easeInOut,
-  //   }
-  // )
+  .fromTo(
+    $main, {
+      autoAlpha: 0,
+    }, {
+      autoAlpha: 1,
+      ease: Power4.easeInOut,
+    }
+  )
   .fromTo(
     $header, {
       autoAlpha: 0,
@@ -239,7 +239,25 @@ window.addEventListener("load", (event) => {
   tltransition.play(0);
 });
 
+// ==================
+// Section fade out
+// ==================
 
+// gsap.utils.toArray(".Welcome-section").forEach((box) => {
+//   gsap.set(box, {
+//     opacity: 1,
+//   });
+//   gsap.to(box, {
+//     scrollTrigger: {
+//       trigger: box,
+//       markers: false,
+//       scrub: 4,
+//       start: "30% 30%",
+//       end: "60% 0%",
+//     },
+//     opacity: 0.6,
+//   });
+// });
 
 // splitting character
 
@@ -326,11 +344,75 @@ gsap.fromTo(
 // Text scroll Animation End
 // =======================
 
+var popoverbox = document.querySelectorAll(".img_work_outer>.popupebox");
+document.addEventListener("mousemove", (obj) => {
+  popoverbox.forEach((pobox) => {
+    var x = obj.clientX;
+    var y = obj.clientY;
+    pobox.style.left = `calc(${obj.clientX}px - 0px)`;
+    pobox.style.top = `calc(${obj.clientY}px + 20px)`;
+  });
+});
 
+var hover = document.querySelectorAll(".img_work_outer");
 
-//============================
-// Text read animation
-// ==========================
+// The functions are cointained inside another one to prevent execution on load.
+hover.forEach((e) => e.addEventListener("mouseover", () => mouseOver(e)));
+hover.forEach((e) => e.addEventListener("mouseout", () => mouseOut(e)));
+
+function mouseOver(e) {
+  e.classList.add("active");
+  var index = 0;
+  const slides = document.querySelectorAll(
+    ".img_work_outer.active > .popupebox .slides"
+  );
+  const classHide = "slides-hidden",
+    count = slides.length;
+  // console.log(count);
+
+  function nextSlide() {
+    slides[index++ % count].classList.add(classHide);
+    slides[index % count].classList.remove(classHide);
+    setTimeout(nextSlide, 2000);
+  }
+  nextSlide();
+
+}
+
+function mouseOut(e) {
+  e.classList.remove("active");
+}
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto
+
+//====== Introduction text highlighter ======//
+// https://codepen.io/hexagoncircle/pen/gOPMwvd
+//====== Introduction text highlighter ======//
+
+// gsap.utils.toArray(".Welcome-section h1 span").forEach((box) => {
+//   gsap.set(box, {
+//     y: 200,
+//     ease: 'power1.out',
+//   });
+//   gsap.to(box, {
+//     scrollTrigger: {
+//       trigger: box,
+//       markers: false,
+//       scrub: 1,
+//       start: "0 100%",
+//       end: "100% 50%"
+//     },
+//     ease: 'power1.out',
+//     y: 0,
+//   });
+// });
+
+// ==================
+// spliting text
+// =================
+
+// https://web.dev/building-split-text-animations/
+
+// https://github.com/jeremyharris/split_lines
 
 const split = new SplitType(".paragraphs", {
   types: "lines",
@@ -388,30 +470,49 @@ window.onresize = function () {
 
 // SVG CALL
 
-
-// gsap.utils.toArray(".section-title .child").forEach((box) => {
+// gsap.utils.toArray(".introduction").forEach((box) => {
 //   gsap.set(box, {
 //     y: 100,
 //     opacity: 0,
-//     skewX: -50,
-//     skewY: 50,
-//     ease: "power1.out",
+//     ease: 'power1.out',
 //   });
 //   gsap.to(box, {
 //     scrollTrigger: {
 //       trigger: box,
-//       markers: true,
+//       markers: false,
 //       scrub: 1,
-//       start: () => "top 70%",
-//       end: () => "top 70%",
+//       start: () => 'top 50%',
+//       end: () => 'top 50%',
 //     },
-//     ease: "power1.out",
+//     ease: 'power1.out',
 //     y: 0,
-//     skewX: 0,
-//     skewY: 0,
-//     opacity: 1,
+//     opacity: 1
 //   });
 // });
+
+gsap.utils.toArray(".section-title .child").forEach((box) => {
+  gsap.set(box, {
+    y: 100,
+    opacity: 0,
+    skewX: -50,
+    skewY: 50,
+    ease: "power1.out",
+  });
+  gsap.to(box, {
+    scrollTrigger: {
+      trigger: box,
+      markers: false,
+      scrub: 1,
+      start: () => "top 70%",
+      end: () => "top 70%",
+    },
+    ease: "power1.out",
+    y: 0,
+    skewX: 0,
+    skewY: 0,
+    opacity: 1,
+  });
+});
 
 function DeveloperImg() {
   const doc = this.getSVGDocument();
@@ -459,6 +560,24 @@ gsap.utils.toArray(".animationimage").forEach((box) => {
     opacity: 1,
   });
 });
+// gsap.utils.toArray("main>section").forEach((box) => {
+//   gsap.set(box, {
+//     y: 10,
+//     scale: 0.9,
+//     ease: "power1.out",
+//   });
+//   gsap.to(box, {
+//     scrollTrigger: {
+//       trigger: box,
+//       markers: false,
+//       scrub: 2,
+//       start: () => "top 80%",
+//       end: () => "top 80%",
+//     },
+//     y: 0,
+//     scale: 1,
+//   });
+// });
 
 
 function WhatIdo() {
@@ -561,7 +680,20 @@ const aesGcmEncrypt = async function (plaintext, password) {
   } catch (err) {}
 } //func aesGcmEncrypt
 
-
+// const aesGcmDecrypt = async function (ciphertext, password) {
+//   try {
+//     const pwUtf8 = new TextEncoder().encode(password);
+//     const pwHash = await crypto.subtle.digest('SHA-256', pwUtf8);
+//     const iv = ciphertext.slice(0, 24).match(/.{2}/g).map(byte => parseInt(byte, 16));
+//     const alg = { name: 'AES-GCM', iv: new Uint8Array(iv) };
+//     const key = await crypto.subtle.importKey('raw', pwHash, alg, false, ['decrypt']);
+//     const ctStr = atob(ciphertext.slice(24));
+//     const ctUint8 = new Uint8Array(ctStr.match(/[\s\S]/g).map(ch => ch.charCodeAt(0)));
+//     const plainBuffer = await crypto.subtle.decrypt(alg, key, ctUint8);
+//     const plaintext = new TextDecoder().decode(plainBuffer);
+//     return plaintext;
+//   } catch (err) { }
+// } //func aesGcmDecrypt
 
 
 //Decrypt and Encrypt
@@ -581,6 +713,10 @@ document.querySelector('.encrypt form').addEventListener('submit', async (event,
 
     const result = await aesGcmEncrypt(text, pass);
 
+    // $result.innerHTML = result ? result : errorMsg;
+    // console.log(text, pass);
+    // console.log(result);
+    // fe0d823a0f642bfbbf3ed895zpxvTo94D35f48O5TxpOBFpqHNBJzg
     if (result === "000000000000000000000000YPBD5utmoXO6CPAB0jurGp2KgWJ6+/gMhW3ihMw=") {
 
         let elements = document.querySelectorAll(".img_work_outer");
@@ -596,56 +732,23 @@ document.querySelector('.encrypt form').addEventListener('submit', async (event,
   }
 });
 
-// =======================
-// On hover slider appear
-// =======================
+// document.getElementsByClassName("viewall")[0].addEventListener("click", validation);
 
-var popoverbox = document.querySelectorAll(".img_work_outer>.popupebox");
-document.addEventListener("mousemove", (obj) => {
-  popoverbox.forEach((pobox) => {
-    var x = obj.clientX;
-    var y = obj.clientY;
-    pobox.style.left = `calc(${obj.clientX}px - 0px)`;
-    pobox.style.top = `calc(${obj.clientY}px + 20px)`;
-  });
-});
-
-var hover = document.querySelectorAll(".img_work_outer");
-
-// The functions are cointained inside another one to prevent execution on load.
-// hover.forEach((e) => e.addEventListener("mouseover", () => mouseOver(e)));
-// hover.forEach((e) => e.addEventListener("mouseout", () => mouseOut(e)));
-hover.forEach((e) => e.addEventListener("mouseenter", () => mouseOver(e)));
-hover.forEach((e) => e.addEventListener("mouseleave", () => mouseOut(e)));
-console.log(hover)
-function mouseOver(e) {
-  e.classList.add("active");
-  slider();
-}
-var timer;
-
-function mouseOut(e) {
-  clearTimeout(timer)
-  e.classList.remove("active");
-}
+// function validation() {
+//   let value = prompt("Please Provide password for access all projects", "");
 
 
-function slider(){
-  
-    var index = 0;
-    const slides = document.querySelectorAll(
-      ".img_work_outer.active > .popupebox .slides"
-    );
-    const classHide = "slides-hidden",
-      count = slides.length;
-      //  console.log(count);
-  
-    function nextSlide() {
-      slides[index++ % count].classList.add(classHide);
-      slides[index % count].classList.remove(classHide);
-      timer = setTimeout(nextSlide, 2000);
-    }
-    
-    nextSlide();
-  
-  }
+
+//   if (value != null && value === "xyz" )  {
+//     console.log("123");
+//    let elements = document.querySelectorAll(".img_work_outer");
+//     elements.forEach((element) => {
+//       element.classList.remove('private');
+//     });
+
+
+
+//   }else{
+//     alert("Please Enter Valid Password")
+//   }
+// }
